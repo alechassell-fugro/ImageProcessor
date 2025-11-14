6a177f19255cecdd5a563d9c2c3654e12e29a2ba
alechassell-fugro <alec.hassell@fugro.com>
2025-11-14 11:14:29 +0800

    Initial commit

---
+++ ImageProcessor/Command.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace ImageProcessor
+{
+    public class Command : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+        public Command(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+        public void Execute(object? parameter) => _execute();
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ ImageProcessor/MainWindowModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace ImageProcessor
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        public string Title { get; set; } = "Image Processor";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
