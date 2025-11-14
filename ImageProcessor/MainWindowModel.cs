6c21ddded8d3cb0cdb7014da43e7405c68a219a4
alechassell-fugro <alec.hassell@fugro.com>
2025-11-14 15:22:00 +0800

    WIP

--- ImageProcessor/MainWindowModel.cs
+++ ImageProcessor/MainWindowModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
@@ -35,8 +36,51 @@ namespace ImageProcessor
                 source.BeginInit();
                 source.UriSource = new Uri(selectedFileName);
                 source.EndInit();
+                
                 ImageSource = source; 
             }
+        }
+
+        public ICommand ProcessImageCommand => new Command(ProcessImage);
+
+        private void ProcessImage()
+        {
+            byte[] bytes = Convert(ImageSource);
+
+            Array.Reverse<byte>(bytes);
+
+            ImageSource = Convert(bytes, ImageSource);
+
+
+        }
+
+        private BitmapSource Convert(string imagePath)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(imagePath);
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad; // Important for releasing file handle after loading
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+
+        private byte[] Convert(BitmapSource bitmapSource)
+        {
+            int stride = (bitmapSource.PixelWidth * bitmapSource.Format.BitsPerPixel + 7) / 8;
+
+            byte[] bytes = new byte[stride * bitmapSource.PixelHeight];
+
+            bitmapSource.CopyPixels(Int32Rect.Empty, bytes, stride, 0);
+
+            return bytes;
+        }
+
+        private BitmapSource Convert(byte[] pixelBytes, BitmapSource original)
+        {
+            int stride = (original.PixelWidth * original.Format.BitsPerPixel + 7) / 8;
+            var bitmapSource = BitmapSource.Create(original.PixelWidth, original.PixelHeight, original.DpiX, original.DpiY, original.Format, original.Palette, pixelBytes, stride);
+            return bitmapSource;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
